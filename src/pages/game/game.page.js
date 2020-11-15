6981399047ea1f0c08c88d2d89e603cfad66c9fb
ryotafuwa <ryota.fuwa.1428@gmail.com>
2020-11-15 12:58:23 +0900

    made jokerWontBeAround cheating system.

--- src/pages/game/game.page.js
+++ src/pages/game/game.page.js
@@ -12,8 +12,24 @@ class Game extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            cards: this.getCards(props.numCards),
+            cards: [],
             gameOver: false,
+        };
+    }
+
+    componentDidMount() {
+        switch(this.props.cheatingSystem) {
+            case "joker won't be around":
+                this.setState(state => ({
+                    cards: this.getCards(this.props.numCards),
+                    gameOver: false,
+                    prevIdx: 0,
+                }));
+            default:
+                this.setState({
+                    cards: this.getCards(this.props.numCards),
+                    gameOver: false,
+                })
         }
     }
 
@@ -24,12 +40,11 @@ class Game extends React.Component {
         for(let i = 0; i < numCards; i++) {
             let numIdx = Math.floor(Math.random() * 13);
             let typeIdx = Math.floor(Math.random() * 4);
-            console.log(numIdx);
 
-            // while(seen[numIdx][typeIdx]) {
-            //     numIdx = (numIdx + 1) % 13;
-            //     typeIdx = (typeIdx + 1) % 4;
-            // }
+            while(seen[numIdx][typeIdx]) {
+                numIdx = (numIdx + 1) % 13;
+                typeIdx = (typeIdx + 1) % 4;
+            }
 
             seen[numIdx][typeIdx] = true;
             cards.push(
@@ -45,15 +60,67 @@ class Game extends React.Component {
         return cards;
     }
 
-    flipCard = idx => {
-        if (idx >= this.state.cards.length) return;
-
+    default(idx) {
         this.setState(state => {
             state.cards[idx].flipped = true;
             return {cards: [...state.cards]};
         }, () => {
             if (this.state.cards[idx].isJoker) setTimeout(this.endGame, 2000);
         });
+    }
+
+    jokerWontBeAround(idx) {
+        console.log(idx);
+        const {prevIdx, cards} = this.state;
+        if(!cards[idx].isJoker) {
+            this.setState(state => {
+                state.cards[idx].flipped = true;
+                return {cards: [...state.cards], prevIdx: idx};
+            }, () => {
+                if (this.state.cards[idx].isJoker) setTimeout(this.endGame, 2000);
+            });
+            return;
+        }
+
+        const neighbors = new Set();
+        const row = Math.floor(prevIdx / 4);
+        const col = prevIdx % 4;
+        for(let i = -1; i <= 1; ++i) {
+            for(let j = -1; j <= 1; ++j) {
+                neighbors.add((row + i) * 4 + col + j);
+            }
+        }
+
+
+        if(neighbors.has(idx)) { //then swap joker with a card not flipped yet.
+            for(let i = 0; i < cards.length; i++) {
+                if(!cards[i].flipped && i !== idx) {
+                    cards[i].isJoker = true;
+                    cards[idx].isJoker = false;
+                    break;
+                }
+            }
+        }
+
+        this.setState(state => {
+            state.cards[idx].flipped = true;
+            return {cards: [...state.cards], prevIdx: idx};
+        }, () => {
+            if (this.state.cards[idx].isJoker) setTimeout(this.endGame, 2000);
+        });
+    }
+
+    flipCard = idx => {
+        if (idx >= this.state.cards.length) return;
+
+        switch(this.props.cheatingSystem) {
+            case "joker won't be around":
+                this.jokerWontBeAround(idx);
+                return;
+            default:
+                this.default(idx);
+                return;
+        }
     }
 
     endGame = () => {
@@ -66,7 +133,7 @@ class Game extends React.Component {
     render() {
         return (
             <div className='game' style={{'backgroundColor': this.state.gameOver ? 'black' : null}}>
-                <BackToMenu/>
+                <BackToMenu />
                 <div className='cards'>
                     {this.state.cards.map(card => (
                         <Card key={card.idx} {...card} onClick={() => this.flipCard(card.idx)}/>
